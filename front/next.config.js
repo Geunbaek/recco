14db9319d28a1a674352716e47f5f441f9a5b7e0
rmsqor103 <rmsqor103@naver.com>
2022-03-31 02:55:18 +0900

    update: base_url

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     // BASE_URL: 'http://localhost:8000',
-    BASE_URL: 'http://www.recco.n-e.kr/api',
+    // BASE_URL: 'http://www.recco.n-e.kr/api',
+    BASE_URL: 'https://peaceful-spire-08369.herokuapp.com',
   },
   images: {
     domains: ['dn5hzapyfrpio.cloudfront.net'],
