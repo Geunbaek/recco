c75188c0242fae643cb723e98548311614fdf57b
baek <rmsqor103@naver.com>
2022-05-04 22:02:36 +0900

    add:node-cron scheduler

--- front/next.config.js
+++ front/next.config.js
@@ -1,3 +1,16 @@
+const cron = require('node-cron');
+const axios = require('axios');
+
+cron.schedule('*/2 * * * *', () => {
+  axios.post(`https://peaceful-spire-08369.herokuapp.com/search/category`, {
+    requestPage: 1,
+    maxItemCountByPage: 5,
+    largeCategory: '스킨케어',
+    smallCategory: '크림',
+    sort: 'id desc',
+  });
+});
+
 module.exports = {
   env: {
     // BASE_URL: 'http://localhost:8000',
